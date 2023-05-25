da321434176a9b58e995cb96eac2b2d6395cc134
zainab-rafea <zainab2108676@miuegypt.edu.eg>
2023-05-26 02:47:28 +0300

    chatttt

---
+++ public/js/chatcircle.js
@@ -0,0 +1,19 @@
+const chatCircle = document.querySelector('.chat-circle');
+
+chatCircle.addEventListener('click', () => {
+  // open chat box code goes here
+// console.log("hiiiii")
+ alert("Chat box should open!");
+//window.location.href = '/chat.ejs';
+// app.get('/open-chat', (req, res) => {
+//     res.render('chat');
+//   });
+
+});
+
+// $(document).ready(function() {
+//     $('.chat-circle').click(function() {
+//       // open chat box code goes here
+//       alert("Chat box opened!");
+//     });
+//   });
