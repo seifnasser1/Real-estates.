ab706b55d48a7af041b0dd5a80b2fac17d54b4c6
seifnasser1 <Seifnasser2001@gmail.com>
2023-05-26 20:17:06 +0300

    Home and About us

--- routes/index.rout.js
+++ routes/index.rout.js
@@ -8,4 +8,8 @@ router.get('/', function(req, res, next) {
   res.render('pages/Home');
 });
 
+router.get('/aboutus', function(req, res, next) {
+  console.log('index.js: GET /');
+  res.render('pages/aboutus');
+});
 export default router;
