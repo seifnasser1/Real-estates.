07b2f386c1702ef4ce09a0e83d0f68b3c30b1847
seifnasser1 <Seifnasser2001@gmail.com>
2023-05-26 20:28:14 +0300

    navigation

--- routes/user.rout.js
+++ routes/user.rout.js
@@ -7,17 +7,5 @@ router.get('/', function(req, res, next) {
   console.log('index.js: GET /');
   res.render('pages/Home');
 });
-router.get('/contactus', function(req, res, next) {
-  console.log('index.js: GET /');
-  res.render('pages/contactus');
-});
-router.get('/register', function(req, res, next) {
-  console.log('index.js: GET /');
-  res.render('pages/register');
-});
-router.get('/addpropirty', function(req, res, next) {
-  console.log('index.js: GET /');
-  res.render('pages/addpropirty');
-});
 
 export default router;
