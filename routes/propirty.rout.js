b67c14579b5ba30873010ebf98bdf18f715ce553
seifnasser1 <Seifnasser2001@gmail.com>
2023-05-26 20:38:41 +0300

    Navigation

---
+++ routes/propirty.rout.js
@@ -0,0 +1,22 @@
+import { Router } from 'express';
+
+const router = Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  console.log('index.js: GET /');
+  res.render('pages/Home');
+});
+router.get('/contactus', function(req, res, next) {
+  console.log('index.js: GET /');
+  res.render('pages/contactus');
+});
+router.get('/register', function(req, res, next) {
+  console.log('index.js: GET /');
+  res.render('pages/register');
+});
+router.get('/addpropirty', function(req, res, next) {
+  console.log('index.js: GET /');
+  res.render('pages/addpropirty');
+});
+export default router;
