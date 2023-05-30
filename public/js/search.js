b6391ffdf2a09709cf9022ff295d4925cc8e98a0
zainab-rafea <zainab2108676@miuegypt.edu.eg>
2023-05-31 02:02:14 +0300

    frontend search

---
+++ public/js/search.js
@@ -0,0 +1,21 @@
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+
+const searchButton = document.querySelector('.search-box button');
+const searchInput = document.querySelector('.search-box input');
+
+searchButton.addEventListener('click', function () {
+    const searchTerm = searchInput.value;
+    console.log(searchTerm);
+    // Do something with the search term here, such as sending it to a server or filtering results on the page.
+});
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("mydb");
+  dbo.collection("customers").findOne({}, function(err, result) {
+    if (err) throw err;
+    console.log(result.name);
+    db.close();
+  });
+});
