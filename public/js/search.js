5e476274df27b843994338a4ddd00854c2f09c4b
zainab-rafea <zainab2108676@miuegypt.edu.eg>
2023-05-31 04:48:12 +0300

    search icon 4aklo bayz

--- public/js/search.js
+++ public/js/search.js
@@ -1,14 +1,14 @@
 var MongoClient = require('mongodb').MongoClient;
-var url = "";
+var url = "mongodb+srv://mostafa2104516:98782363@cluster0.fn8yy9v.mongodb.net/sasa?retryWrites=true&w=majority";
 
-const searchButton = document.querySelector('.search-box button');
-const searchInput = document.querySelector('.search-box input');
+// const searchButton = document.querySelector('.search-box button');
+// const searchInput = document.querySelector('.search-box input');
 
-searchButton.addEventListener('click', function () {
-    const searchTerm = searchInput.value;
-    console.log(searchTerm);
-    // Do something with the search term here, such as sending it to a server or filtering results on the page.
-});
+// searchButton.addEventListener('click', function () {
+//     const searchTerm = searchInput.value;
+//     console.log(searchTerm);
+//     // Do something with the search term here, such as sending it to a server or filtering results on the page.
+// });
 /////////////////////////////////////////
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
@@ -19,3 +19,12 @@ MongoClient.connect(url, function(err, db) {
     db.close();
   });
 });
+
+function openFormfind() {
+  console.log("hi here")
+  if(document.getElementById("myFormfind").style.display=="none")
+  document.getElementById("myFormfind").style.display = "block";
+  else
+  document.getElementById("myFormfind").style.display = "none";
+ 
+}
