825922a114fc7a5c3b8dcf3693a35c1ace4a80c8
MIMO902 <115392508+MIMO902@users.noreply.github.com>
2023-06-04 04:07:37 +0300

    editting

--- routes/user.rout.js
+++ routes/user.rout.js
@@ -31,7 +31,6 @@ router.get('/register', (req, res) => {
 })
 router.get('/propirty', (req, res) => {
   Propirty.find()
-<<<<<<< HEAD
   .then(result => {
     res.render('pages/All', { Propirty: result,  user: (req.session.user === undefined ? "" : req.session.user)});
   })
@@ -42,42 +41,18 @@ router.get('/propirty', (req, res) => {
 
 
 router.get('/:id',(req, res) => {
-=======
-    .then(result => {
-      res.render('pages/All', { Propirty: result });
-    })
-    .catch(err => {
-      console.log(err);
-    });
-});
-
-
-
-router.get('/:id', (req, res) => {
->>>>>>> 795399aa4de35631ffaea11216865ac67cce72f6
   var query = { "_id": req.params.id };
   User.find(query)
     .then(result => {
       console.log(result[0]);
-<<<<<<< HEAD
       res.render('pages/profile', { User: result[0] , user: (req.session.user === undefined ? "" : req.session.user)});
-=======
-      res.render('pages/profile', { User: result[0] });
->>>>>>> 795399aa4de35631ffaea11216865ac67cce72f6
     })
     .catch(err => {
       console.log(err);
     });
 });
-<<<<<<< HEAD
-router.post('/search',Search);
-router.post('/signup-action',validation,signup);
-router.post('/login-action',login);
-export default router ;
-=======
 router.post('/search', Search);
 router.post('/signup-action', validation, signup);
 router.post('/login-action', login);
 router.post('/send-message',sendMes);
 export default router;
->>>>>>> 795399aa4de35631ffaea11216865ac67cce72f6
