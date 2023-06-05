55fba5cdc0c2e0e333b7d6933e90927086565885
Monad Khaled <monad2005657@miuegypt.edu.eg>
2023-06-06 00:50:56 +0300

    trial

--- controllers/contactus.controller.js
+++ controllers/contactus.controller.js
@@ -1,35 +1,68 @@
-import Contactusmsg from '../models/contactus.model.js';
-import __dirname from '../app.js'  //
+import Contactusmsg from "../models/contactus.model.js";
+import { body, validationResult } from "express-validator";
+import __dirname from "../app.js"; //
 
 //import fileUpload from "express-fileupload"; //
 
+const contactUsValidation = [
+  body("cname").notEmpty().withMessage("Name is required"),
+  body("cmail").isEmail().withMessage("Invalid email"),
+  body("cphone").isMobilePhone().withMessage("Phone is required"),
+  body("cloc").notEmpty().withMessage("Location is required"),
+  body("cmes").notEmpty().withMessage("Message is required"),
+];
 
-const contact = async (req, res) => {
-
-
+const addcontactusmsg = async (req, res) => {
   // if (flagg > 0) {
   //    return ;
   //  }
+  const errors = validationResult(req);
 
+  if (!errors.isEmpty()) {
+    console.log("YP");
+    console.log(
+      errors
+        .array()
+        .map((ele) => ele.msg)
+        .join(" ")
+    );
+    res.render("pages/contactus", {
+      message: errors
+        .array()
+        .map((ele) => ele.msg)
+        .join(" - "),
+      user: req.session.user === undefined ? "" : req.session.user,
+    });
+    return res.status(400).end();
+  }
 
-     const contactusmsg = new Contactusmsg({
-      cname: req.body.cname,
-      cphone: req.body.cphone,
-      cloc: req.body.cloc,
-      cmail: req.body.cmail,
-     cmes:req.body.cmes,
+  const contactusmsg = new Contactusmsg({
+    name: req.body.cname,
+    phno: req.body.cphone,
+    loc: req.body.cloc,
+    email: req.body.cmail,
+    ask: req.body.cmes,
+  });
 
-     
-     });
-     contactusmsg.save()
-       .then(result => {
-         res.render('pages/contctus');
-       })
-       .catch(err => {
-         console.log(err);
-       });
+  contactusmsg
+    .save()
+    .then((result) => {
+      console.log(result);
+      res.redirect("/contactus");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+const getAllMessages = async (req, res) => {
+  // also it needs pagination, in case of it get bigger
+  Contactusmsg.find({})
+    .then((results) => {
+      // res.render("")
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
 
-   
-  };
-
-  export { addcontactusmsg};
+export { addcontactusmsg, contactUsValidation, getAllMessages };
